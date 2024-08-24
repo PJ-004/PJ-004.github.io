f6d8cf7a8102f83546dcffc84e1382d0b53edb45
PJ-004 <114767771+PJ-004@users.noreply.github.com>
2024-08-24 06:09:13 -0700

    Added an Education section to resumePage.js

--- Scripts/resumePage.js
+++ Scripts/resumePage.js
@@ -8,7 +8,12 @@ const resume = "<p> <h2> Work Experience </h2> </p>\
                 <p> Developed a python script for a close relative that extracts the warnings from a System Verilog logfile. \
                  The script searches through a log file and extracts warning messages from them after looping through every line. \
                  This script required thorough knowledge of the python programming language. \
-                 It also drastically improved the workflow of the employees by automating an otherwise tedious task. </p>";
+                 It also drastically improved the workflow of the employees by automating an otherwise tedious task. </p>\
+<p> <h2> Education </h2> </p> \
+<p> <h3> September 2022 - Present | University of California - Santa Cruz </h3> </p>\
+<p> Bachelor's of Science in Computer Engineering </p>\
+<p> <h3> September 2012 - July 2022 | Delhi Public School - Bangalore East </h3> </p>\
+<p> PCMC Stream </p>";
 
 function resumePage() {
     document.getElementById('body').innerHTML = resume;
