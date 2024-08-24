382892d0a21e607fd0127c074b0d81ceb6cf0a12
PJ-004 <114767771+PJ-004@users.noreply.github.com>
2024-08-24 06:22:46 -0700

    Update resumePage.js

--- Scripts/resumePage.js
+++ Scripts/resumePage.js
@@ -4,7 +4,7 @@ const resume = "<p> <h2> Work Experience </h2> </p>\
                 <p> Interned at QuicHub Innovations India as a Software Developer. Migrated their data from the older urql to GraphQL.  \
                  I worked with React, TypeScript, Next.js, and GraphQL to help with designing their frontend software. \
                  Worked on a WhatsApp business account that can send and receive messages built using Go </p>\
-                <p> <h3> July 2023 </h3> </p>\
+                <p> <h3> July 2023 - August 2023 </h3> </p>\
                 <p> Developed a python script for a close relative that extracts the warnings from a System Verilog logfile. \
                  The script searches through a log file and extracts warning messages from them after looping through every line. \
                  This script required thorough knowledge of the python programming language. \
