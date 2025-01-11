1cbaf4a78be1ab16b7724bcad1b340d3becbccf9
Pranav Jha <pjha2@ucsc.edu>
2025-01-10 22:43:01 -0800

    Removed unecessary lines from Scripts/aboutPpage.js

--- Scripts/aboutPage.js
+++ Scripts/aboutPage.js
@@ -1,15 +1,5 @@
 const newDate = new Date();
 
-function findAge(month, dayOfMonth) {
-    if (month == 0 && dayOfMonth <= 12) {
-        var age = newDate.getFullYear() - 2003;
-    }
-
-    age = newDate.getFullYear() - 2004;
-
-    return age;
-}
-
 function aboutMe() {
     location.href = 'index.html';
 }
